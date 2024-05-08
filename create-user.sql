096743019f776a7a223c2bbc7a5504b0eca013e8
SHCHERBANV <shcherbanvasyl@gmail.com>
2024-05-07 19:01:33 -0600

    Solution

--- create-user.sql
+++ create-user.sql
@@ -1,10 +1,10 @@
-CREATE USER 'backup'@'%' IDENTIFIED BY 'P@ssw0rd';
+CREATE USER 'vr'@'%' IDENTIFIED BY '8262';
 
 -- configure minimum requored permissions for performing a db backup using mysqldump: 
-GRANT SELECT, LOCK TABLES, SHOW VIEW, PROCESS ON *.* TO 'backup'@'%';
+GRANT SELECT, LOCK TABLES, SHOW VIEW, PROCESS ON *.* TO 'vr'@'%';
 
 -- configure required permissions to restore a database 
-GRANT ALL ON ShopDBReserve.* TO 'backup'@'%';
+GRANT ALL ON ShopDBReserve.* TO 'vr'@'%';
 
 -- configure required permissions to restore only data
-GRANT INSERT, LOCK TABLES, ALTER ON ShopDBDevelopment.* TO 'backup'@'%';
+GRANT INSERT, LOCK TABLES, ALTER ON ShopDBDevelopment.* TO 'vr'@'%';
--- task.sh
+++ task.sh
@@ -1,1 +1,19 @@
 #! /bin/bash
+
+# Reading environment variables
+DB_USER="vr"
+DB_PASSWORD="8262"
+
+# Backup path set
+BACKUP_PATH="C:\\Users\\shche\\mate\\mysql.backup"
+
+# Ensure the directory exists
+mkdir -p "$BACKUP_PATH"
+
+# Full backup of ShopDB and restore to ShopDBReserve
+mysqldump -u"$DB_USER" -p"$DB_PASSWORD" --no-create-db ShopDB >"$BACKUP_PATH/ShopDB.sql"
+mysql -u"$DB_USER" -p"$DB_PASSWORD" ShopDBReserve <"$BACKUP_PATH/ShopDB.sql"
+
+# Backup only data from ShopDB and restore to ShopDBDevelopment
+mysqldump -u"$DB_USER" -p"$DB_PASSWORD" --no-create-info --skip-add-drop-table ShopDB >"$BACKUP_PATH/ShopDBDataOnly.sql"
+mysql -u"$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment <"$BACKUP_PATH/ShopDBDataOnly.sql"
